6173bb71fff12d7ba114796cf8352c4d743c016e
Olivia-Roberta Dogbey <robertadogbey@gmail.com>
2025-08-16 14:43:44 +0000

    Fix: made some changes related to deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/',
+  base: './',
   
   build: {
-    outDir: "dist",
+    outDir: "build",
     chunkSizeWarningLimit: 2000,
     manifest: true,
     rollupOptions: {
